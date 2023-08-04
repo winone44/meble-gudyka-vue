9caf012074b51684888737ebb28e559e14fa3da2
winone44 <winone1208@gmail.com>
2023-08-04 11:48:47 +0200

    bootstrap css update

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,43 +1,44 @@
 <template>
-  <div class="main-wrapper vh-100" style="background-color: #508bfc">
-    <b-container class="py-5 h-100">
-      <b-row class="d-flex justify-content-center align-items-center h-100">
-        <b-col cols="12" class="col-md-8 col-md-8 col-lg-6 col-xl-5">
-          <b-card class="shadow-2-strong" style="border-radius: 1rem">
-            <b-card-body class="p-5 text-center">
-              <h3 class="mb-5">Zaloguj się</h3>
-              <div class="form-outline mb-4">
-                <b-form @submit.prevent="onSubmit">
-                  <b-form-group
-                      label="Email:"
-                      type="email"
-                      required
-                  >
-                    <b-form-input
-                        v-model="email"
-                        placeholder="Wpisz swój email"
-                    ></b-form-input>
-                  </b-form-group>
-                  <b-form-group
-                      label="Hasło:"
-                  >
-                    <b-form-input
-                        v-model="password"
-                        type="password"
+  <div id="login-form">
+    <div class="main-wrapper vh-100" style="background-color: #508bfc">
+      <b-container class="py-5 h-100">
+        <b-row class="d-flex justify-content-center align-items-center h-100">
+          <b-col cols="12" class="col-md-8 col-md-8 col-lg-6 col-xl-5">
+            <b-card class="shadow-2-strong" style="border-radius: 1rem">
+              <b-card-body class="p-5 text-center">
+                <h3 class="mb-5">Zaloguj się</h3>
+                <div class="form-outline mb-4">
+                  <b-form @submit.prevent="onSubmit">
+                    <b-form-group
+                        label="Email:"
+                        type="email"
                         required
-                        placeholder="Wpisz swoje hasło"
-                    ></b-form-input>
-                  </b-form-group>
-                  <b-button type="submit" class="btn-lg btn-block mt-5" variant="primary">Zaloguj się</b-button>
-                </b-form>
-              </div>
-            </b-card-body>
-          </b-card>
-        </b-col>
-      </b-row>
-    </b-container>
+                    >
+                      <b-form-input
+                          v-model="email"
+                          placeholder="Wpisz swój email"
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                        label="Hasło:"
+                    >
+                      <b-form-input
+                          v-model="password"
+                          type="password"
+                          required
+                          placeholder="Wpisz swoje hasło"
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-button type="submit" class="btn-lg btn-block mt-5" variant="primary">Zaloguj się</b-button>
+                  </b-form>
+                </div>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -68,6 +69,21 @@ export default {
 </script>
 
 
-<style scoped src="bootstrap/dist/css/bootstrap.css"></style>
-<style scoped src="bootstrap-vue/dist/bootstrap-vue.css"></style>
+<style scoped lang="scss">
+:deep() {
+  @import '../assets/scss/main.scss';
+}
+</style>
 
+<style scoped>
+#login-form {
+  margin: 0;
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: left;
+  background-color: #fff;
+}
+</style>
